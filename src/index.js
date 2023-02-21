70df3d179a4684ecbbd52eaacad8c876bec9df8d
Sandeep Shelke <sandeep@nano-frames.com>
2023-02-21 14:11:55 +0000

    connect with railway deployed server

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import GlobalStyles from './styles';
+import Pages from './pages';
+
+const client = new ApolloClient({
+  uri: "https://odyssey-lift-off-server-production-98f4.up.railway.app/",
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <GlobalStyles />
+    <Pages />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
