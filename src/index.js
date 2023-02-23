006c84c55da1bc5d16df29637daa9e8a4099461b
Sandeep Shelke <sandeep@nano-frames.com>
2023-02-23 14:33:54 +0000

    Adds module page

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import GlobalStyles from './styles';
 import Pages from './pages';
-
+// "https://odyssey-lift-off-server-production-98f4.up.railway.app/"
 const client = new ApolloClient({
-  uri: "https://odyssey-lift-off-server-production-98f4.up.railway.app/",
+  uri: process.env.SERVER_URI || "http://localhost:4000/",
   cache: new InMemoryCache(),
 });
 
