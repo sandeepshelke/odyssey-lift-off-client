d8b75d960bcb24161fb1091ebf8a577c8830c6d0
Sandeep Shelke <sandeep@nano-frames.com>
2023-02-24 10:40:16 +0000

    add hosted server

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import GlobalStyles from './styles';
 import Pages from './pages';
-// "https://odyssey-lift-off-server-production-98f4.up.railway.app/"
-const server_url = process.env.SERVER_URI || "http://localhost:4000/";
+const server_url = "https://odyssey-lift-off-server-production-98f4.up.railway.app/"
+//const server_url = "http://localhost:4000/";
 console.log(`connecting to url: ${server_url}`)
 const client = new ApolloClient({
   uri: server_url,
